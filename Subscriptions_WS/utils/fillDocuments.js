1e0ee052738d945b5d2e00bca8b8d1f6a8858498
OrelMv <62357351+OrelMv@users.noreply.github.com>
2021-04-26 16:00:50 +0300

    Update fillDocuments.js

--- Subscriptions_WS/utils/fillDocuments.js
+++ Subscriptions_WS/utils/fillDocuments.js
@@ -1,66 +1,60 @@
 require('../configs/subscriptionsDataBase')
 const Movie = require('../models/schemas/moviesSchema')
 const Member = require('../models/schemas/membersSchema')
+const Subscription = require('../models/schemas/subscriptionsSchema')
 const utils = require('./utils')
 
 let getMembersData = async()=> {
     let membersData = await utils.getData("https://jsonplaceholder.typicode.com/users");
-    let arr = []
+
     membersData.data.forEach(member => {
-        arr.push({
+
+        let memberObj = new Member({
             name: member.name,
             email: member.email,
             city: member.address.city
         })
+
+        memberObj.save(err=>{
+            if(err){
+                console.log(err)
+            } else {
+                let subscriptionObj = new Subscription({
+                    memberId: memberObj._id,
+                    movies: []
+                })
+                subscriptionObj.save(err => {
+                    if(err){
+                        console.log(err);
+                    }
+                })
+            }
+        })
+        
     })
-    return arr
+
 }
 
 let getMoviesData = async() => {
     let moviesData = await utils.getData("https://api.tvmaze.com/shows")
-    let arr = []
-    for(let i = 0; i < 30; i++){
-        arr.push({
+    for(let i = 0; i < 15; i++){
+
+        let movieObj = new Movie({
             name: moviesData.data[i].name,
             genres: moviesData.data[i].genres,
             image: moviesData.data[i].image.medium,
             premiered: moviesData.data[i].premiered
         })
+        movieObj.save(err => {
+            if(err){
+                console.log(err)
+            }
+        })
     }
-    return arr
+
     
 }
 
-let putCollections = async() => {
-    let movies = await getMoviesData();
-    movies.forEach(movie=> {
-        let newMovie = new Movie({
-            name: movie.name,
-            genres: movie.genres,
-            image: movie.image,
-            premiered: movie.premiered
-        })
-        newMovie.save(err=>{
-            if(err){
-                console.log(err)
-            }
-        });
-    })
+getMembersData()
+getMoviesData()
 
-    let members = await getMembersData();
-    members.forEach(member=> {
-        let newMember = new Member({
-            name: member.name,
-            email:member.email,
-            city: member.city
-        })
-
-        newMember.save(err=>{
-            if(err){
-                console.log(err)
-            }
-        })
-    })
-}
-
-putCollections();
